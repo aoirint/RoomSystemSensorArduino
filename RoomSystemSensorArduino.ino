aff0ee36dec6647f20098d294c9a8b6a19b0c233
aoirint <aoirint@gmail.com>
2020-11-11 22:55:45 +0900

    celsius temperature for LM61CIZ

--- RoomSystemSensorArduino.ino
+++ RoomSystemSensorArduino.ino
@@ -1,4 +1,6 @@
 #include <ArduinoJson.h>
+#define TEMPERATURE_VCC 5.0
+#define TEMPERATURE_OFFSET -11.3
 
 void setup() {
   Serial.begin(115200);
@@ -18,10 +20,15 @@ void loop() {
   int lightRaw = analogRead(A0);
   int temperatureRaw = analogRead(A1);
 
+  // for LM61CIZ, V_o = (+10mV/@C x T @C) + 600mV 
+  float temperatureCelsius = ((((float) temperatureRaw) / 1023.0f * TEMPERATURE_VCC) - 0.6f) / 0.01f;
+  temperatureCelsius += TEMPERATURE_OFFSET;
+
   data.clear();
   data["type"] = "sensor";
   data["light"] = lightRaw;
   data["temperature"] = temperatureRaw;
+  data["temperatureCelsius"] = temperatureCelsius;
   data["nowButtonPressed"] = nowButtonPressed;
   data["waitButtonPressed"] = waitButtonPressed;
   data["doorIsOpen"] = doorIsOpen;
